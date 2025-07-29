c52d67cc10a3005e69044fdcef4c3cbe09e1577b
P Avilash <123006421+avilash01@users.noreply.github.com>
2025-07-29 14:06:32 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,51 +1,66 @@
 const socket = io();
 const peer = new Peer();
 
-let myVideo = document.createElement('video');
-myVideo.muted = true;
 let localStream;
+const peers = {};
 
-const lawyerVideo = document.getElementById('lawyerVideo');
-const clientVideo = document.getElementById('clientVideo');
 const messages = document.getElementById('messages');
+const videoContainer = document.querySelector('.video-container');
 
 navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
     localStream = stream;
-
-    // Set own video
-    if (user === "lawyer") {
-        lawyerVideo.srcObject = stream;
-    } else {
-        clientVideo.srcObject = stream;
-    }
+    addVideoStream(createVideoElement(user), stream);
 
     peer.on('call', call => {
         call.answer(stream);
+        const video = createVideoElement("Peer");
         call.on('stream', remoteStream => {
-            if (user === "lawyer") {
-                clientVideo.srcObject = remoteStream;
-            } else {
-                lawyerVideo.srcObject = remoteStream;
-            }
+            addVideoStream(video, remoteStream);
         });
     });
 
-    socket.emit('join-room', { room, user });
+    socket.emit('join-room', { room, user, peerId: peer.id });
 
-    socket.on('user-joined', data => {
-        if (data.user !== user) {
-            const call = peer.call(peer.id, stream);
-            call.on('stream', remoteStream => {
-                if (user === "lawyer") {
-                    clientVideo.srcObject = remoteStream;
-                } else {
-                    lawyerVideo.srcObject = remoteStream;
-                }
-            });
-        }
+    socket.on('user-connected', ({ peerId }) => {
+        connectToNewUser(peerId, stream);
     });
 });
 
+function connectToNewUser(peerId, stream) {
+    const call = peer.call(peerId, stream);
+    const video = createVideoElement("Peer");
+    call.on('stream', remoteStream => {
+        addVideoStream(video, remoteStream);
+    });
+    call.on('close', () => {
+        video.remove();
+    });
+    peers[peerId] = call;
+}
+
+function createVideoElement(label) {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'video-box';
+
+    const title = document.createElement('h2');
+    title.innerText = label;
+
+    const video = document.createElement('video');
+    video.autoplay = true;
+    video.playsInline = true;
+
+    wrapper.appendChild(title);
+    wrapper.appendChild(video);
+    videoContainer.appendChild(wrapper);
+    return video;
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => video.play());
+}
+
+// Chat
 document.getElementById('chatForm').addEventListener('submit', (e) => {
     e.preventDefault();
     const msg = document.getElementById('chatInput').value;
@@ -72,4 +87,21 @@ function addMessage(msg, sender) {
 function leaveRoom() {
     socket.emit('leave-room', { room, user });
     window.location.href = '/';
+}
+
+// Screen Share
+function startScreenShare() {
+    navigator.mediaDevices.getDisplayMedia({ video: true }).then(screenStream => {
+        for (let peerId in peers) {
+            const sender = peers[peerId].peerConnection.getSenders().find(s => s.track.kind === 'video');
+            if (sender) sender.replaceTrack(screenStream.getVideoTracks()[0]);
+        }
+
+        screenStream.getVideoTracks()[0].onended = () => {
+            for (let peerId in peers) {
+                const sender = peers[peerId].peerConnection.getSenders().find(s => s.track.kind === 'video');
+                if (sender) sender.replaceTrack(localStream.getVideoTracks()[0]);
+            }
+        };
+    });
 }
